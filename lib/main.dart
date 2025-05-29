bf709da0150d1c0ed2dfe26f6d469fd82970045c
Bryan <freyrecanbryan@gmail.com>
2025-05-28 23:10:57 -0600

    files names changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'DetailPaga.dart';
+import 'detail_paga.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
